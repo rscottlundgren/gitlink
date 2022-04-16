d1d9cd16a2d9dcafb50bda8b9d6499ee590d6968
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-04-16 16:23:20 -0400

    Swapped a `/` for `?`

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -13,7 +13,7 @@ export const searchUsers = async (text) => {
 		q: text,
 	});
 
-	const response = await github.get(`/search/users/${params}`);
+	const response = await github.get(`/search/users?${params}`);
 	return response.data.items;
 };
 
