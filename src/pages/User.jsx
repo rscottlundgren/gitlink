4321f1635c2dcc011298712bcf68ef7a553b8674
R. Scott Lundgren <rscottlundgren@gmail.com>
2022-04-16 15:29:44 -0400

    Import/Extract `GithubActions`, Update `useEffect`
    - Import `GithubActions` file
    - Extract `getUser`, `getUserRepos` from `GithubActions` file
    - Remove `getUser`, `getUserRepos` extract from Context API
    - Update `useEffect` hook to use `dispatch`,  `GithubActions` functions

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,16 +5,24 @@ import Spinner from '../components/layout/Spinner';
 import GithubContext from '../context/github/GithubContext';
 import { useParams } from 'react-router-dom';
 import RepoList from '../components/repos/RepoList';
+import { getUser, getUserRepos } from '../context/github/GithubActions';
 
 function User() {
-	const { getUser, user, isLoading, getUserRepos, repos } =
-		useContext(GithubContext);
+	const { user, isLoading, repos, dispatch } = useContext(GithubContext);
 
 	const params = useParams();
 
 	useEffect(() => {
-		getUser(params.login);
-		getUserRepos(params.login);
+		dispatch({ type: 'SET_LOADING' });
+		const getUserData = async () => {
+			const userData = await getUser(params.login);
+			dispatch({ type: 'GET_USER', payload: userData });
+
+			const userRepoData = await getUserRepos(params.login);
+			dispatch({ type: 'GET_REPOS', payload: userRepoData });
+		};
+
+		getUserData();
 	}, []);
 
 	const {
